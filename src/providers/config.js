87ff4acffa3aabdc776996656f77fb08beb64c03
ingenio0 <ingenio0@ingenio0-MS-7817>
2017-07-10 15:53:10 -0500

    config.js

--- src/providers/config.js
+++ src/providers/config.js
@@ -9,5 +9,8 @@
 //export let WS_BASE = "http://192.168.2.72/override/app/services/";
 
 
-export let URL_BASE = "http://fluzfluzdev.com/";
-export let WS_BASE = "http://fluzfluzdev.com/override/app/services/";
+export let URL_BASE = "http://fluzfluzweb.localhost/";
+export let WS_BASE = "http://fluzfluzweb.localhost/override/app/services/";
+
+//export let URL_BASE = "http://fluzfluzdev.com/";
+//export let WS_BASE = "http://fluzfluzdev.com/override/app/services/";
